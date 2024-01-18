6a9af45f2b4a7d2bbd3dc0c6317425193ab55103
brandon chubb <brandon.chubb@kellogg.northwestern.edu>
2024-01-18 01:13:14 +0000

    schoo 5-7 but 5 code is off

---
+++ school-5-insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO students (first_name, 
+    last_name,
+    email) 
+    VALUES ("Ben",
+    "Block",
+    ben@kellogg.northwestern.edu");
+
+
+-- We let sql handle id so we don't enter it in the code
---
+++ school-6-update.sql
@@ -0,0 +1,6 @@
+UPDATE students
+SET email = "ben@hey.com"
+WHERE id = 1;
+
+
+-- ALways add WHERE so that it doesn't update every row in that whole column
